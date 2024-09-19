a273feadd02f78254a06a5174745c48a0c2683e1
Himanshu Madhesshiya <155805250+CODEWITHHMM@users.noreply.github.com>
2024-09-19 18:35:34 +0530

    update

---
+++ dom/script.js
@@ -0,0 +1,104 @@
+////////////////////   document.getElementById ///////////////////////
+
+
+// let firstImage = document.getElementById("first");
+// firstImage.src = "https://cdn.pixabay.com/photo/2024/04/04/13/17/ai-generated-8675104_960_720.png";
+
+
+////////////////////   document.getElementByClassName ///////////////////////
+
+
+// let cname = document.getElementsByClassName("mainImage");
+
+// for (let i = 0; i < cname.length; i++) {
+//     cname[i].src = "https://cdn.pixabay.com/photo/2024/04/04/13/17/ai-generated-8675104_960_720.png";
+//     console.log(`value of ${i} image has changed`);
+
+// }
+
+
+////////////////////   document.getElementByTagName ///////////////////////
+
+
+// let tname= document.getElementsByTagName("p");
+// tname[0].innerText = "himanshu";
+
+
+
+
+////////////////////   document.querySelector ///////////////////////
+
+let qselect=document.querySelector("#description");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
